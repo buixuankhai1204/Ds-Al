c56daac48f66596a97605cd4cbee77fc5c372d43
khaibuixuan2 <khaibuixuan2@gmail.com>
2022-10-24 18:26:40 +0700

    linked list

---
+++ Ds-Al/LinkedList/Node.cs
@@ -0,0 +1,43 @@
+﻿namespace Ds_Al.LinkedList;
+
+public class Node
+{
+    public int data;
+    public Node next = null;
+
+    public Node(int data)
+    {
+        this.data = data;
+        this.next = null;
+    }
+
+    public void InsertEndNode(Node node, int data)
+    {
+        while (node.next != null)
+        {
+            node = node.next;
+        }
+        Node newNode = new Node(data);
+        node.next = newNode;
+    }
+
+    public void InsertHeadNode(Node node, int data)
+    {
+        Node newNode = new Node(data);
+        newNode.next = node;
+        node = newNode;
+    }
+
+    public void PrintNodeValue(Node node)
+    {
+        int count = 0;
+        while (node != null)
+        {
+            Console.WriteLine(node.data);
+            count++;
+            node = node.next;
+        }
+        Console.WriteLine("Length: " + count);
+
+    }
+}
---
+++ Ds-Al/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Ds_Al.LinkedList;
+
+Node newnode = new Node(5);
+newnode.InsertEndNode(newnode,7);
+newnode.InsertEndNode(newnode,1);
+newnode.InsertEndNode(newnode,4);
+newnode.InsertHeadNode(newnode, 2);
+newnode.PrintNodeValue(newnode);
